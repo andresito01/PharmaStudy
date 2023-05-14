393c7df9731e286db56a22dc573f6d71d869912a
lamphancs <lamphancs@gmail.com>
2023-05-14 04:25:27 -0700

    update viewNavigation view

--- src/ViewNavigation.js
+++ src/ViewNavigation.js
@@ -1,37 +1,76 @@
 import { Link } from "react-router-dom";
+import { Button, Box } from '@mui/material';
 
 function ViewNavigation() {
 
   const LinkCSS = {
     color: "white",
-    fontSize: "2rem",
+    fontSize: "2rem"
   };
 
   return (
     <div
       className="ViewNavigationPage"
       style={{
-        width: "100vw",
-        height: "100vh",
+        width: "100%",
+        height: "100%",
         display: "flex",
         flexDirection: "column",
         alignItems: "center",
         justifyContent: "center",
-        gap: "2rem",
+        gap: "30px",
       }}
     >
-      <Link style={LinkCSS} to={"/janehopkinsdoctorlogin"}>
-        Jane Hopkins Doctor
+      <Box
+        m="20px"
+        display="flex"
+        flexDirection="column"
+        alignItems="center"
+        height="100vh"
+        width = "400px"
+        marginTop="200px"
+       >
+      <Link to={"/janehopkinsdoctorlogin"}>
+        <Box width="100%" marginBottom="22px">
+          <Button 
+          color="warning"
+          variant="outlined"
+          sx={{ padding: "12px 70px" }}
+          size = "large">
+            Jane Hopkins Doctor</Button>
+        </Box>
       </Link>
-      <Link style={LinkCSS} to={"/janehopkinsadminlogin"}>
-        Jane Hopkins Admin
+      <Link to={"/janehopkinsadminlogin"}>
+      <Box width="100%" marginBottom="22px">
+      <Button 
+        color="warning"
+        variant="outlined"
+        sx={{ padding: "12px 76px"}}
+        size = "large">
+          Jane Hopkins Admin</Button>
+        </Box>
       </Link>
-      <Link style={LinkCSS} to={"/fdalogin"}>
-        FDA
+      <Link to={"/fdalogin"}>
+      <Box width="100%" marginBottom="22px">
+      <Button 
+        color="warning"
+        variant="outlined"
+        sx={{ padding: "12px 125px" }}
+        size = "large">
+          FDA</Button>
+          </Box>
       </Link>
-      <Link style={LinkCSS} to={"/bavarialogin"}>
-        Bavaria
+      <Link to={"/bavarialogin"}>
+      <Box width="100%" marginBottom="22px">
+      <Button 
+        color="warning"
+        variant="outlined"
+        sx={{ padding: "12px 115px" }}
+        size = "large">
+          Bavaria</Button>
+          </Box>
       </Link>
+      </Box>
     </div>
   );
 }
